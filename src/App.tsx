86b64d754ccf9b40a6d967d7539432fee9953edd
Alen Basar <alenbasar.dev@gmail.com>
2025-04-16 02:56:49 +1000

    feat: add global state for theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import type React from 'react'
+import { ThemeProvider } from './context/ThemeContext'
+import { GlobalStyles } from './styles/GlobalStyles'
+import Navbar from './Components/Navbar/Navbar.tsx'
+import ContactsBrowser from './Components/Contact/ContactsBrowser.tsx'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const App: React.FC = () => {
+    return (
+        <ThemeProvider>
+            <GlobalStyles />
+            <Navbar />
+            <div
+                style={{
+                    padding: '20px',
+                    display: 'flex',
+                    justifyContent: 'center',
+                }}
+            >
+                <ContactsBrowser titleBy="status" />
+            </div>
+        </ThemeProvider>
+    )
 }
 
 export default App
